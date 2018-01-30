87d33f14f6ee2083d7f680a4184559a21acf5c92
Stone <827443469@qq.com>
2018-01-30 20:57:21 +0800

    lua调用c#

---
+++ Assets/0_Test/Scs/Test01.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01
+{
+    public static void hello()
+    {
+        Debug.Log("static method hello");
+    }
+
+    public void hello2()
+    {
+        Debug.Log("model method hello2");
+    }
+
+    public void hello3(string s)
+    {
+        Debug.Log("hello3 + " + s);
+    }
+
+    public string hello4()
+    {
+        string res = "hello4";
+        Debug.Log(res);
+        return res;
+    }
+
+}
