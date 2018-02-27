e5a580b3450796c7721e8099daa4f33d3db64612
Stone <827443469@qq.com>
2018-02-27 21:09:55 +0800

    add example and stu call

---
+++ Assets/My/Scripts/Test_Call.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class Test_Call : MonoBehaviour {
+
+    LuaEnv luaenv;
+
+	// Use this for initialization
+	void Start () {
+        luaenv = new LuaEnv();
+        luaenv.DoString("require 'hellolua'");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+[XLua.LuaCallCSharp]
+public class CallClass
+{
+    public void test()
+    {
+        Debug.Log("this is CallClass test");
+    }
+}
